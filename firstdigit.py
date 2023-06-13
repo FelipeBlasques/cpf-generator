f3ece6afcab2e40a2201ab7b0d9969a9a145aaf6
Felipe Blasques <130731174+FelipeBlasques@users.noreply.github.com>
2023-06-13 13:04:52 -0300

    Update firstdigit.py

--- firstdigit.py
+++ firstdigit.py
@@ -2,7 +2,7 @@
 
 MULTIPLIERS = [10, 9, 8, 7, 6, 5, 4, 3, 2]
 
-cpf_input = input("Enter CPF (numbers only): ")
+cpf_input = input('Enter CPF (numbers only): ')
 cpf = [int(digit) for digit in cpf_input]
 
 result = []
@@ -18,4 +18,4 @@ digit = digit % 11
 if digit > 9:
     digit = 0
 
-print(f"O primeiro dígito do CPF é: {digit}")
+print(f'The first digit of the CPF is: {digit}')
